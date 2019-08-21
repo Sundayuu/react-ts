327da2800faabdc50b579816b37ba011fd09bdf8
zhangh <frankhuhu@163.com>
2019-08-21 16:53:58 +0800

    404页面点击后返回首页

--- src/components/notFound/index.tsx
+++ src/components/notFound/index.tsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { history } from 'utils';
 import './notFound.less';
 export default class NotFound extends Component<any, any> {
+  goBack = () => {
+    history.push('/home');
+  };
   render() {
     return (
-      <div className="notFound">
+      <div className="notFound" onClick={() => this.goBack()}>
         <img src={require('assets/img/404.png@2o.png')} alt="404" />
       </div>
     );
